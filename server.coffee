3ffb1d082f9d6af4dc1ee73f9160197a61c070e5
Thomas Brandl <thobra@gmail.com>
2017-05-03 07:34:59 +0200

    Saving averaged data to csv every 5m

--- server.coffee
+++ server.coffee
@@ -8,13 +8,43 @@ cors = require 'cors'
 express = require 'express'
 app = express()
 http = require('http').Server(app)
-
+fs = require 'fs'
 app.use cors()
 
+csvWriter = require 'csv-write-stream'
+writer = csvWriter()
+writeStream = fs.createWriteStream 'airquality.csv',  flags: 'a'
+writer.pipe writeStream
+
 cp = require('child_process')
+
+Number::map = (in_min, in_max, out_min, out_max) ->
+    Math.round (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+# airsensor setup to poll every 20s
+poll_every = 20
+# Send every x seconds (average of collected values)
+send_every = 5 * 60
+data_points_to_average = send_every / poll_every
+data_array = []
 # Expect airsensor in same folder
 spw = cp.spawn './airsensor', ['-j']
 
+sumarray = (previous, current) -> current += previous
+
+log_data = (json) ->
+  value = json.e[0].v
+  data_array.push value
+  #console.log "Length is #{data_array.length}"
+  if data_array.length % data_points_to_average is 0
+    sum = data_array.reduce sumarray
+    avg = sum / data_array.length
+    air_index = avg.map(450, 2000, 100, 0)
+    #console.log "Logging #{air_index} as avg of", data_array
+    writer.write
+      timestamp: json.e[0].t
+      index: air_index
+    data_array = []
 
 app
   .get '/stream', (req, res) ->
@@ -26,8 +56,9 @@ app
 
     spw.stdout.on 'data',  (data) ->
         out = data.toString()
+        json  = JSON.parse out
         lines = out.split "\n"
-        #console.log lines.length, lines
+        log_data json
         res.write "data: #{lines[0]}\n\n" unless lines.length > 2
 
     spw.on 'close', (code) ->
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var app, cors, cp, express, http, spw;
+var app, cors, cp, csvWriter, data_array, data_points_to_average, express, fs, http, log_data, poll_every, send_every, spw, sumarray, writeStream, writer;
 
 cors = require('cors');
 
@@ -8,11 +8,55 @@ app = express();
 
 http = require('http').Server(app);
 
+fs = require('fs');
+
 app.use(cors());
+
+csvWriter = require('csv-write-stream');
+
+writer = csvWriter();
+
+writeStream = fs.createWriteStream('airquality.csv', {
+  flags: 'a'
+});
+
+writer.pipe(writeStream);
 
 cp = require('child_process');
 
+Number.prototype.map = function(in_min, in_max, out_min, out_max) {
+  return Math.round((this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+};
+
+poll_every = 20;
+
+send_every = 5 * 60;
+
+data_points_to_average = send_every / poll_every;
+
+data_array = [];
+
 spw = cp.spawn('./airsensor', ['-j']);
+
+sumarray = function(previous, current) {
+  return current += previous;
+};
+
+log_data = function(json) {
+  var air_index, avg, sum, value;
+  value = json.e[0].v;
+  data_array.push(value);
+  if (data_array.length % data_points_to_average === 0) {
+    sum = data_array.reduce(sumarray);
+    avg = sum / data_array.length;
+    air_index = avg.map(450, 2000, 100, 0);
+    writer.write({
+      timestamp: json.e[0].t,
+      index: air_index
+    });
+    return data_array = [];
+  }
+};
 
 app.get('/stream', function(req, res) {
   res.writeHead(200, {
@@ -20,9 +64,11 @@ app.get('/stream', function(req, res) {
     'Cache-control': 'no-cache'
   });
   spw.stdout.on('data', function(data) {
-    var lines, out;
+    var json, lines, out;
     out = data.toString();
+    json = JSON.parse(out);
     lines = out.split("\n");
+    log_data(json);
     if (!(lines.length > 2)) {
       return res.write("data: " + lines[0] + "\n\n");
     }
