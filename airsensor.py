34d8021f5e828e31711e5cee82c873c4510645cf
Thomas Brandl <thobra@gmail.com>
2017-12-29 13:58:58 +0100

    2nd read was required

--- airsensor.py
+++ airsensor.py
@@ -6,6 +6,8 @@ import usb.util
 from time import sleep
 from struct import unpack_from, calcsize
 
+DEBUG = False
+
 def mapNum(value, in_min, in_max, out_min, out_max):
     return int((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
@@ -22,7 +24,6 @@ if dev is None:
 if dev.is_kernel_driver_active(0):
     try:
             dev.detach_kernel_driver(0)
-            print ("kernel driver detached")
     except usb.core.USBError as e:
             sys.exit("Could not detach kernel driver: %s" % str(e))
 
@@ -32,7 +33,6 @@ usb.util.claim_interface(dev, 0)
 
 # get an endpoint instance
 cfg = dev.get_active_configuration()
-
 intf = cfg[(0,0)]
 
 ep = usb.util.find_descriptor(
@@ -45,21 +45,33 @@ ep = usb.util.find_descriptor(
 
 assert ep is not None
 
-def read():
+def read(flush=False):
     try:
-        return dev.read(0x81, 0x10, 1000)
-    except usb.core.USBError:
-        print("Could not read from USB")
-        sys.exit()
+        data = dev.read(0x81, 0x0010)
+        log("USB returns {} with length {} on read".format(data, len(data)))
+        return data
+    except usb.core.USBError as e:
+        print("Could not read from USB", e)
+        if(flush is False):
+            print("exiting")
+            sys.exit()
 
+def log(msg):
+    if(DEBUG is True):
+        print(msg)
 #Flush
-read()
+ret = read(flush=True)
+if(ret is None):
+    sleep(1)
+    # Sometimes a 2nd read is required
+    ret = read(flush=True)
+
 
 ret = dev.write(0x02, "\x40\x68\x2a\x54\x52\x0a\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40", 1000)
-#print("USB returns {} on write".format(ret))
+log("USB returns {} on write".format(ret))
 
 data = read()
-#print("USB returns {} with length {} on read".format(data, len(data)))
+
 assert len(data) > 0
 toc, = unpack_from('<H', data, 2)
 
